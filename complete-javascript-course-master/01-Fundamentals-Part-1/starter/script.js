b2673162c1aa320ae235fafafc8eba1c97003892
Jimmy Thomas <jthomas302@gmail.com>
2023-10-20 14:13:20 -0400

    add js and assignments script files

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,8 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+let firstName = "Jimmy";
+console.log("Hi my name is " + firstName);
+
+let myFirstJob = "Developer";
+let myCurrentJob = "Teacher";
