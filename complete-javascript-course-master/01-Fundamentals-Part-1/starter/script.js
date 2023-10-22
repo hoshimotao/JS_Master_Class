5e2e29b86012e6356ef0dbfa5f3ba5f178c48785
Jimmy Thomas <jthomas302@gmail.com>
2023-10-22 19:00:01 -0400

    practice with operators, assignments, mutations, comparisons

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,8 +1,57 @@
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+// // let js = "amazing";
+// // console.log(40 + 8 + 23 - 10);
 
-let firstName = "Jimmy";
-console.log("Hi my name is " + firstName);
+// // let firstName = "Jimmy";
+// // console.log("Hi my name is " + firstName);
 
-let myFirstJob = "Developer";
-let myCurrentJob = "Teacher";
+// // let myFirstJob = "Developer";
+// // let myCurrentJob = "Teacher";
+
+// console.log(typeof true);
+// console.log(typeof 23);
+// console.log(typeof "hello");
+// let jonas = "Jonas";
+
+// console.log(jonas);
+
+// // Dynamic typing is just changing the variable assignment
+// jonas = 23;
+
+// console.log(jonas);
+
+// let year = undefined;
+// console.log(typeof year);
+// console.log(year);
+
+// console.log(jimmyAge); // simple subtraction
+// console.log(jimmyAge, jimmyAge ** 2); //to the power of 2
+
+// const firstName = "Jimmy";
+// const lastName = "Thomas";
+// console.log(firstName + " " + lastName);
+
+// ASSIGNMENT OPERATORS
+// let x = 5 + 10; // 15
+
+// x += 10; // x = x + 10 = 25
+// x *= 4; // x = x * 4 = 100
+// x++; // x = x + 1
+// x--; // x = x - 1
+
+// console.log(x);
+// console.log();
+
+// const currentYear = 2023;
+// const jimmybday = 1991;
+// const samBday = 2001;
+
+// const ageJimmy = currentYear - jimmybday;
+// const ageSam = currentYear - samBday;
+
+// // console.log(ageJimmy > ageSam);
+// console.log(ageSam >= 18);
+
+// const isFullAge = ageSam >= 18;
+// console.log(isFullAge);
+
+// console.log(currentYear - 1991 > currentYear - 2001);
