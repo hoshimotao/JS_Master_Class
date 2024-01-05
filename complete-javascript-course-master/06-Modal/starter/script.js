921817c21a624732a6379bab77417172d25a9854
Jimmy Thomas <jthomas302@gmail.com>
2024-01-04 21:09:30 -0500

    enable the modal to be closed by listening for the esc key only when the modal is open

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -13,11 +13,15 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-console.log(btnsOpenModal);
-
 // ADD AN EVENT LISTENER TO EACH OF THE MODAL BUTTONS
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', openModal);
 }
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !overlay.classList.contains('hidden')) {
+    closeModal();
+  }
+});
