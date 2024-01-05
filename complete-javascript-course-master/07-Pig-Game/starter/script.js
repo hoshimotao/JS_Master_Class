f63f9c5a91ca5d8a6301101e344da059f0205f57
Jimmy Thomas <jthomas302@gmail.com>
2024-01-04 22:43:26 -0500

    Now when a player rolls a 1, it will now be player two's turn

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,11 +1,53 @@
 'use strict';
 
 // Declare DOM Elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
 // Starting Conditions
+const scores = [0, 0];
 score0El.textContent = 0;
 score1El.textContent = 0;
+let currentScore = 0;
+let activePlayer = 0;
+
 diceEl.classList.add('hidden');
+
+// Rolling dice functionality
+
+// NEW
+btnNew.addEventListener('click', function () {});
+
+// ROLL
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  diceEl.src = `dice-${dice}.png`;
+  diceEl.classList.remove('hidden');
+
+  if (dice !== 1) {
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    // switch player & and clear current score
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    switchPlayer();
+  }
+});
+
+const switchPlayer = function () {
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
